d873b0e7b7afc82e6c71506932b98f2f1da7ab61
Your Name In Single Quotes <your email here in single quotes>
2020-04-09 20:04:25 +0300

    lab7 update

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 'use strict';
+
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
-
 app.get('/', (request, response) => {
-    response.send('Home Page!');
+    response.send('Home Page !');
 });
+
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
+app.get('/trails', trailsHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
-////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 function locationHandler(request, response) {
     const city = request.query.city;
@@ -27,7 +29,7 @@ function locationHandler(request, response) {
             const locationData = new Location(city, geoData);
             response.status(200).json(locationData);
         })
-        .catch((err) => errorHandler(err, request, response));
+        .catch((error) => errorHandler(error, request, response));
 }
 
 function Location(city, geoData) {
@@ -36,66 +38,55 @@ function Location(city, geoData) {
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
 }
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
+/////////////////////////////////////////////
 function weatherHandler(request, response) {
     superagent(
         `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.search_query}&key=${process.env.WEATHER_API_KEY}`
-        //`https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&country=US&//start_date=2020-04-02&end_date=2020-04-03&key=API_KEY`
     )
-        .then((weatherRes) => {
-            const weatherSummaries = weatherRes.body.data.map((day) => {
+        .then((weatherData) => {
+            const locationData = weatherData.body.data.map((day) => {
                 return new Weather(day);
             });
-            response.status(200).json(weatherSummaries);
+            response.status(200).json(locationData);
         })
-        .catch((err) => errorHandler(err, request, response));
+        .catch((error) => errorHandler(error, request, response))
 }
 function Weather(day) {
     this.forecast = day.weather.description;
-    this.time = new Date(day.valid_date).toString().slice(0, 15);
+    this.time = new Date(day.valid_date).toString().split(' ').slice(0, 4).join(' ');
 }
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-function Trails(trial) {
-    this.name = trial.name;
-    this.location = trial.location;
-    this.length = trial.length;
-    this.stars = trial.stars;
-    this.star_votes = trial.startVotes;
-    this.summary = trial.summary;
-    this.trail_url = trial.url;
-    this.conditions = trial.conditionStatus;
-    this.condition_date = traildata.conditionDate.toString().slice(0, 10);
-    this.condition_time = traildata.conditionDate.toString().slice(11, 19);
-}
-
-app.get('/trails', trailsHandler);
+//////////////////////////////////////////
 function trailsHandler(request, response) {
     superagent(
-        `https://www.hikingproject.com/data/get-trails?${request.query.trails.latitude}&&lon=${request.query.trails.longitude}&key=${process.env.TRAIL_API_KEY}`
-
+        `https://www.hikingproject.com/data/get-trails?lat=${request.query.latitude}&lon=${request.query.longitude}&maxDistance=400&key=${process.env.TRAIL_API_KEY}`
     )
-        .then((trailsRes) => {
-            const trailSummaries = trailsRes.body.data.map((trial) => {
-                return new Trails(trial);
-            });
-            response.status(200).json(trailSummaries);
-        })
-        .catch((err) => errorHandler(err, request, response));
+    .then((trialData) => {
+        const TData = trialData.body.trails.map((Trail) => {
+            return new Trails(Trail);
+        });
+        response.status(200).json(TData);
+    })
+    .catch((error) => errorHandler(error, request, response))
 }
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
+function Trails(Trail) {
+    this.name = Trail.name;
+    this.location = Trail.location;
+    this.length = Trail.length;
+    this.stars = Trail.stars;
+    this.star_votes = Trail.starVotes;
+    this.summary = Trail.summary;
+    this.trail_url = Trail.trail_url;
+    this.conditions = Trail.conditionStatus;
+    this.condition_date =Trail.conditionDate.slice(0,10);
+    this.condition_time =Trail.conditionDate.slice(12,19);
+}
+//////////////////////////////////////////
 function notFoundHandler(request, response) {
-    response.status(404).send('Sorry, something went wrong');
-}
-
-
-function errorHandler(error, request, response) {
+    response.status(404).send('NOT FOUND!');
+  }
+  
+  function errorHandler(error, request, response) {
     response.status(500).send(error);
-}
+  }
 app.listen(PORT, () => console.log(`the server is up and running on ${PORT}`));
